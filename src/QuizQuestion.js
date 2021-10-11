6d5eccfdf77766442fbabf98c7947bb125727c23
binwelbeck <binwelbeck1@gmail.com>
2021-10-11 18:12:39 +0300

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,8 +5,10 @@ class QuizQuestion extends Component {
         return (
             <main>
             <section>
-              <p>// instruction text goes here</p>
+              <p>// instruction text goes here
+
               {this.props.quiz_question.instruction_text}
+              </p>
             </section>
             <section className="buttons">
               <ul>
@@ -23,4 +25,4 @@ class QuizQuestion extends Component {
 
 }
 
-export default  QuizQuestion ;
+export default QuizQuestion 
