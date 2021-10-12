c37cee87c2cdcdd623c6701c64c6423e411959f6
binwelbeck <binwelbeck1@gmail.com>
2021-10-12 18:08:55 +0300

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,27 +1,32 @@
-import React, {Component} from 'react';
-import QuizQuestion from './QuizQuestion';
+import React, { Component } from "react";
+import QuizEnd from "./QuizEnd";
+import QuizQuestion from "./QuizQuestion";
 
-let quizData = require('./quiz_data.json')
+let quizData = require("./quiz_data.json");
 
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { quiz_position:1 };
+  }
 
- class Quiz extends Component {
-
-    constructor(props){
-        super(props)
-
-        this.state= 
-            {quiz_position:1}
-        }
-    
-
-    render() {
-        return (
-            <QuizQuestion
-                quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
-            ></QuizQuestion>
-        )
-    }
-    
+  render() {
+    const isQuizEnd = (this.state.quiz_position - 1) == quizData.quiz_questions.length;
+    return (
+      <div>
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+            // quiz_position={quiz_position}
+          ></QuizQuestion>
+        )}
+      </div>
+    );
+  }
 }
-export default Quiz 
+export default Quiz;
